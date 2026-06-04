e0cd06b0f4e75b9bc55fd85941a44e0f3e68c8f7
Pascoal Silva <sou.jorge.atlas2023@gmail.com>
2026-06-04 03:10:26 +0100

    Atualizando o JWTMiddleware

--- app/Http/Middleware/JwtMiddleware.php
+++ app/Http/Middleware/JwtMiddleware.php
@@ -8,28 +8,21 @@ use Tymon\JWTAuth\Facades\JWTAuth;
 
 class JwtMiddleware
 {
-    public function handle(Request $request, Closure $next)
+    public function handle($request, Closure $next)
     {
         try {
-
             $token = $request->cookie('jwt_token');
 
             if (!$token) {
                 return redirect('/login');
             }
 
-            $user = JWTAuth::setToken($token)->authenticate();
+            $request->headers->set('Authorization', 'Bearer ' . $token);
 
-            if (!$user) {
-                return redirect('/login');
-            }
-
-            auth()->setUser($user);
+            $user = JWTAuth::parseToken()->authenticate();
 
         } catch (\Exception $e) {
-
             return redirect('/login');
-
         }
 
         return $next($request);
