94f86c0bb348df6ad124cdafe5bb01ef27ce2b00
Pascoal Silva <sou.jorge.atlas2023@gmail.com>
2026-06-04 22:36:47 +0100

    Atualizando a migration carteira

--- database/migrations/0008_create_wallets_table.php
+++ database/migrations/0008_create_wallets_table.php
@@ -13,10 +13,33 @@ return new class extends Migration
     {
         Schema::create('carteiras', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->foreignUuid('usuario_id')->unique()->constrained('perfis')->onDelete('cascade');
-            $table->decimal('saldo', 15, 2)->default(0);
-            $table->string('tipo')->default('usuario'); // usuario | plataforma
-            $table->string('moeda')->default('AOA');
+
+            // Nullable para permitir carteira da plataforma
+            $table->foreignUuid('usuario_id')
+                ->nullable()
+                ->unique()
+                ->constrained('perfis')
+                ->nullOnDelete();
+
+            $table->string('iban_virtual', 21)
+                ->nullable()
+                ->unique();
+
+            $table->unsignedBigInteger('numero_conta_interno')
+                ->nullable()
+                ->unique();
+
+            $table->decimal('saldo', 15, 2)
+                ->default(0);
+
+            $table->string('tipo')
+                ->default('usuario'); // usuario | plataforma
+
+            $table->string('moeda')
+                ->default('AOA');
+
+            $table->index('tipo');
+
             $table->timestamps();
         });
     }
