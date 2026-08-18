28de21605ef67b5e43907a5e2b45f849c1a73842
Pascoal Silva <sou.jorge.atlas2023@gmail.com>
2026-08-18 17:36:39 +0100

    refactor(pagina_inicial): adicionando o ícone de cadeado svg

--- resources/views/home/inicio.blade.php
+++ resources/views/home/inicio.blade.php
@@ -575,7 +575,9 @@
     <article class="relative bg-surface-container-lowest text-on-surface rounded-lg p-8 md:p-10 pt-14 shadow-xl">
       <div class="absolute -top-6 -left-6 w-16 h-16 bg-primary-container text-on-primary-container rounded-full border-[6px] border-secondary-container flex items-center justify-center text-xl font-medium">2</div>
       <div class="mb-6 text-secondary-container">
-        <span class="material-symbols-outlined text-5xl">lock</span>
+        <svg class="w-12 h-12" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z"/>
+  </svg>
       </div>
       <h3 class="font-headline-md text-xl md:text-2xl font-bold uppercase mb-4">RETIDO</h3>
       <p class="font-body-md text-body-md text-on-surface-variant leading-relaxed text-pretty">O valor fica guardado na Skilla.</p>
