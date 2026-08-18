f9666d85f035c10f8b057786937151a1e9b9e4bb
Pascoal Silva <sou.jorge.atlas2023@gmail.com>
2026-08-18 17:37:59 +0100

    refactor(pagina_inicial): adicionando o ícone de verificado  svg

--- resources/views/home/inicio.blade.php
+++ resources/views/home/inicio.blade.php
@@ -587,7 +587,9 @@
     <article class="relative bg-surface-container-lowest text-on-surface rounded-lg p-8 md:p-10 pt-14 shadow-xl">
       <div class="absolute -top-6 -left-6 w-16 h-16 bg-primary-container text-on-primary-container rounded-full border-[6px] border-secondary-container flex items-center justify-center text-xl font-medium">3</div>
       <div class="mb-6 text-secondary-container">
-        <span class="material-symbols-outlined text-5xl">verified</span>
+        svg class="w-12 h-12" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z"/>
+  </svg>
       </div>
       <h3 class="font-headline-md text-xl md:text-2xl font-bold uppercase mb-4">APROVADO</h3>
       <p class="font-body-md text-body-md text-on-surface-variant leading-relaxed text-pretty">Você recebe e aprova o trabalho.</p>
