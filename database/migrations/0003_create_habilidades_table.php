6d5235345f8d7b6798d9fa0e9f535741089127dd
Pascoal Silva <sou.jorge.atlas2023@gmail.com>
2026-09-03 20:18:50 +0100

    refactor(migration_habilidades): Alterando a categoria (text), para aplicar as subcategorias

--- database/migrations/0003_create_habilidades_table.php
+++ database/migrations/0003_create_habilidades_table.php
@@ -13,9 +13,19 @@ return new class extends Migration
     {
         Schema::create('habilidades', function (Blueprint $table) {
             $table->uuid('id')->primary();
+
             $table->string('nome')->unique();
-            $table->string('categoria');
+
+            // cada habilidade pertence a uma categoria (idealmente uma SUBCATEGORIA)
+            $table->uuid('categoria_id');
+
             $table->timestamps();
+
+            $table->index('categoria_id');
+            $table->foreign('categoria_id')
+                ->references('id')
+                ->on('categorias')
+                ->restrictOnDelete();
         });
     }
 
